00003455f9d19369328240f56fbeb5ebf0b0d9cb
Caitlyn Griffing <caitlyn.griffing@gmail.com>
2022-02-24 14:15:20 -0600

    graphql connected

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,13 +16,21 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+async function startGraphQl() {
+  await server.start().then(res => {
+    server.applyMiddleware({ app })
+    console.log(server)
+  })
+}
+startGraphQl();
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`graphql: http://localhost:${PORT}${server.graphqlPath}`)
 });
