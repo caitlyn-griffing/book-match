b4a15d9d3ea5f5a080b4b412c56f2a43b5202250
Caitlyn Griffing <caitlyn.griffing@gmail.com>
2022-03-22 20:24:05 -0500

    add login user mutation

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -1,7 +1,37 @@
+import { gql } from '@apollo/client'
+
 // LOGIN_USER will execute the loginUser mutation set up using Apollo Server
+export const LOGIN_USER = gql`
+    mutation loginUser( $email: String!, $password: String! ) {
+    login(email: $email, password: $password){
+        token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                bookID
+                title
+                description
+                authors
+                link
+                image
+                }
+            }
+        }  
+    }
+`;
 
 // ADD_USER will execute the addUser mutation
 
+
+
 // SAVE_BOOK will execute the saveBook mutation
 
+
 // REMOVE_BOOK 
+
+
+
+
